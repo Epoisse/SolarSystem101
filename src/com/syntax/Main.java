71595d946fcb2b414b2f794acf2777685486328c
Epoisse <anton.anokhin.qa@gmail.com>
2022-05-16 15:29:59 -0400

    Solar system basic ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	SolarSystem factoX=new SolarSystem();
+        factoX.moon = "4";
+        factoX.planet = "2";
+        factoX.stars = "10000";
+        factoX.sun = "1";
+    }
+}
